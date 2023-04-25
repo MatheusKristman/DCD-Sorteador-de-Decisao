2e3236cc9f5ae4962aa44eb62310c59963a50a8a
MatheusKristman <kristman058@gmail.com>
2023-04-25 18:20:02 -0300

    fixed shuffle area

--- src/pages/components/ShufflerContent.jsx
+++ src/pages/components/ShufflerContent.jsx
@@ -1,189 +1,230 @@
-import React, { useRef, useEffect } from "react";
-import useShufflerStore from "../../stores/useShufflerStore";
+import React, { useRef, useEffect } from 'react';
+import useShufflerStore from '../../stores/useShufflerStore';
 import Shuffle from './Shuffle';
 
 const ShufflerContent = () => {
-  const values = useShufflerStore((state) => state.values);
-  const inputValue = useShufflerStore((state) => state.inputValue);
+    const values = useShufflerStore((state) => state.values);
+    const inputValue = useShufflerStore((state) => state.inputValue);
 
-  const handleInputValue = useShufflerStore((state) => state.handleInputValue);
-  const addChoice = useShufflerStore((state) => state.addChoice);
-  const handleAddChoice = () => {
-    if (inputValue === "") {
-      return;
-    }
+    const handleInputValue = useShufflerStore(
+        (state) => state.handleInputValue
+    );
+    const addChoice = useShufflerStore((state) => state.addChoice);
+    const handleAddChoice = () => {
+        if (inputValue === '') {
+            return;
+        }
 
-    addChoice();
-  };
+        addChoice();
+    };
 
-  const handleAddChoiceKeyboard = (e) => {
-    if (e.key === "Enter") {
-      if (inputValue === "") {
-        return;
-      }
+    const handleAddChoiceKeyboard = (e) => {
+        if (e.key === 'Enter') {
+            if (inputValue === '') {
+                return;
+            }
 
-      addChoice();
-    }
-  }
+            addChoice();
+        }
+    };
 
-  const handleEditValue = useShufflerStore((state) => state.handleEditValue);
-  const editable = useShufflerStore((state) => state.editable);
-  const acceptEdit = useShufflerStore((state) => state.acceptEdit);
-  const declineEdit = useShufflerStore((state) => state.declineEdit);
+    const handleEditValue = useShufflerStore((state) => state.handleEditValue);
+    const editable = useShufflerStore((state) => state.editable);
+    const acceptEdit = useShufflerStore((state) => state.acceptEdit);
+    const declineEdit = useShufflerStore((state) => state.declineEdit);
 
-  const acceptEditKeyboard = (e) => {
-    if (e.key === 'Enter') {
-      if (e.target.value === '') {
-        return;
-      }
+    const acceptEditKeyboard = (e) => {
+        if (e.key === 'Enter') {
+            if (e.target.value === '') {
+                return;
+            }
 
-      acceptEdit();
-    }
-  }
+            acceptEdit();
+        }
+    };
 
-  const deleteChoice = useShufflerStore((state) => state.deleteChoice);
+    const deleteChoice = useShufflerStore((state) => state.deleteChoice);
 
-  const isShuffling = useShufflerStore((state) => state.isShuffling);
-  const handleShuffle = useShufflerStore((state) => state.handleShuffle);
-  const loading = useShufflerStore((state) => state.loading);
- 
-  const updateResult = useShufflerStore((state) => state.updateResult);
+    const isShuffling = useShufflerStore((state) => state.isShuffling);
+    const handleShuffle = useShufflerStore((state) => state.handleShuffle);
+    const loading = useShufflerStore((state) => state.loading);
 
-  const choiceRef = useRef([]);
-  const editInput = useRef([]);
+    const updateResult = useShufflerStore((state) => state.updateResult);
 
-  const shuffleValues = () => {
-    if (values.length === 0) {
-      return;
-    }
+    const choiceRef = useRef([]);
+    const editInput = useRef([]);
 
-    loading();
-    const randomIndex = Math.floor(Math.random() * values.length);
-    const choiceSelected = values[randomIndex].value;
-    updateResult(choiceSelected);
-    handleShuffle();
-  }
+    const shuffleValues = () => {
+        if (values.length === 0) {
+            return;
+        }
 
-  useEffect(() => {
-    if(isShuffling) {
-      scrollTo(0, 0, 'smooth');
-      document.documentElement.style.overflowY = 'hidden';
-    } else {
-      document.documentElement.style.overflowY = 'unset';
-    }
-  }, [isShuffling]);
+        loading();
+        const randomIndex = Math.floor(Math.random() * values.length);
+        const choiceSelected = values[randomIndex].value;
+        updateResult(choiceSelected);
+        handleShuffle();
+    };
 
-  return (
-    <div className="shuffler">
-      {isShuffling && <Shuffle />}
-      <div className="shuffler__container">
-        <div className="shuffler__container__input-box">
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputValue}
-            onKeyDown={handleAddChoiceKeyboard}
-            className="shuffler__container__input-box__input"
-          />
-          <button
-            onClick={handleAddChoice}
-            type="button"
-            className="shuffler__container__input-box__btn"
-          >
-            Adicionar
-          </button>
+    useEffect(() => {
+        if (isShuffling) {
+            scrollTo(0, 0, 'smooth');
+            document.documentElement.style.overflowY = 'hidden';
+        } else {
+            document.documentElement.style.overflowY = 'unset';
+        }
+    }, [isShuffling]);
+
+    return (
+        <div className='shuffler'>
+            {isShuffling && <Shuffle />}
+            <div className='shuffler__container'>
+                <div className='shuffler__container__input-box'>
+                    <input
+                        type='text'
+                        value={inputValue}
+                        onChange={handleInputValue}
+                        onKeyDown={handleAddChoiceKeyboard}
+                        className='shuffler__container__input-box__input'
+                    />
+                    <button
+                        onClick={handleAddChoice}
+                        type='button'
+                        className='shuffler__container__input-box__btn'
+                    >
+                        Adicionar
+                    </button>
+                </div>
+
+                <div className='shuffler__container__choices-container'>
+                    <div className='shuffler__container__choices-container__box wrapper'>
+                        {values.length > 0 ? (
+                            values.map((choice, index) => (
+                                <div
+                                    key={`${choice.value}-${index + 1}`}
+                                    className='shuffler__container__choices-container__box__choice'
+                                    ref={(el) =>
+                                        (choiceRef.current[index] = el)
+                                    }
+                                >
+                                    {choice.isEditable ? (
+                                        <>
+                                            <button
+                                                className='shuffler__container__choices-container__box__choice__edit'
+                                                type='button'
+                                                onClick={() =>
+                                                    acceptEdit(index)
+                                                }
+                                            >
+                                                <i className='fa-solid fa-check' />
+                                            </button>
+                                            <button
+                                                className='shuffler__container__choices-container__box__choice__delete'
+                                                type='button'
+                                                onClick={() =>
+                                                    declineEdit(index)
+                                                }
+                                            >
+                                                <i className='fa-solid fa-xmark' />
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <button
+                                                onClick={() => {
+                                                    editable(index);
+                                                    editInput.current[
+                                                        index
+                                                    ].focus();
+                                                }}
+                                                className='shuffler__container__choices-container__box__choice__edit'
+                                                type='button'
+                                            >
+                                                <i className='fa-solid fa-pencil' />
+                                            </button>
+                                            <button
+                                                className='shuffler__container__choices-container__box__choice__delete'
+                                                type='button'
+                                                onClick={() => {
+                                                    choiceRef.current[
+                                                        index
+                                                    ].style.animation =
+                                                        'bounce-out 0.8s ease-in-out forwards';
+
+                                                    setTimeout(() => {
+                                                        deleteChoice(index);
+                                                    }, 800);
+                                                }}
+                                            >
+                                                <i className='fa-regular fa-trash-can' />
+                                            </button>
+                                        </>
+                                    )}
+
+                                    <textarea
+                                        ref={(el) =>
+                                            (editInput.current[index] = el)
+                                        }
+                                        autoCorrect='off'
+                                        autoComplete='off'
+                                        readOnly={!choice.isEditable}
+                                        rows={
+                                            choice.value.length > 15 ||
+                                            choice.newValue.length > 15 ||
+                                            choice.isEditable
+                                                ? 2
+                                                : 1
+                                        }
+                                        value={
+                                            choice.isEditable
+                                                ? choice.newValue
+                                                : choice.value
+                                        }
+                                        placeholder={
+                                            choice.isEditable
+                                                ? 'Selecione para editar'
+                                                : ''
+                                        }
+                                        onChange={(e) =>
+                                            handleEditValue(
+                                                e.target.value,
+                                                index
+                                            )
+                                        }
+                                        onKeyDown={(e) => {
+                                            if (e.key === 'Enter') {
+                                                if (choice.newValue === '') {
+                                                    return;
+                                                }
+
+                                                acceptEdit(index);
+                                            }
+
+                                            if (e.key === 'Escape') {
+                                                declineEdit(index);
+                                            }
+                                        }}
+                                        className='shuffler__container__choices-container__box__choice__value'
+                                    />
+                                </div>
+                            ))
+                        ) : (
+                            <h3 className='shuffler__container__choices-container__box__tip-empty'>
+                                Adicione sua opção acima para decidir
+                            </h3>
+                        )}
+                    </div>
+
+                    <button
+                        className='shuffler__container__choices-container__btn'
+                        onClick={shuffleValues}
+                    >
+                        Decidir
+                    </button>
+                </div>
+            </div>
         </div>
-
-        <div className="shuffler__container__choices-container">
-          <div className="shuffler__container__choices-container__box wrapper">
-            {values.map((choice, index) => (
-              <div
-                key={`${choice.value}-${index + 1}`}
-                className="shuffler__container__choices-container__box__choice"
-                ref={(el) => (choiceRef.current[index] = el)}
-              >
-                {choice.isEditable ? (
-                  <>
-                    <button
-                      className="shuffler__container__choices-container__box__choice__edit"
-                      type="button"
-                      onClick={() => acceptEdit(index)}
-                    >
-                      <i className="fa-solid fa-check" />
-                    </button>
-                    <button
-                      className="shuffler__container__choices-container__box__choice__delete"
-                      type="button"
-                      onClick={() => declineEdit(index)}
-                    >
-                      <i className="fa-solid fa-xmark" />
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <button
-                      onClick={() => {
-                        editable(index);
-                        editInput.current[index].focus();
-                      }}
-                      className="shuffler__container__choices-container__box__choice__edit"
-                      type="button"
-                    >
-                      <i className="fa-solid fa-pencil" />
-                    </button>
-                    <button
-                      className="shuffler__container__choices-container__box__choice__delete"
-                      type="button"
-                      onClick={() => {
-                        choiceRef.current[index].style.animation = "bounce-out 0.8s ease-in-out forwards";
-
-                        setTimeout(() => {
-                          deleteChoice(index);
-                        }, 800);
-                      }}
-                    >
-                      <i className="fa-regular fa-trash-can" />
-                    </button>
-                  </>
-                )}
-
-                <textarea
-                  ref={(el) => editInput.current[index] = el}
-                  autoCorrect="off"
-                  autoComplete="off"
-                  readOnly={!choice.isEditable}
-                  rows={
-                    choice.value.length > 15 || choice.newValue.length > 15 || choice.isEditable
-                      ? 2
-                      : 1
-                  }
-                  value={choice.isEditable ? choice.newValue : choice.value}
-                  placeholder={choice.isEditable ? "Selecione para editar" : ""}
-                  onChange={(e) => handleEditValue(e.target.value, index)}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      if (choice.newValue === '') {
-                        return;
-                      }
-                
-                      acceptEdit(index);
-                    }
-
-                    if (e.key === 'Escape') {
-                      declineEdit(index);
-                    }
-                  }}
-                  className="shuffler__container__choices-container__box__choice__value"
-                />
-              </div>
-            ))}
-          </div>
-
-          <button className="shuffler__container__choices-container__btn" onClick={shuffleValues}>Decidir</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ShufflerContent;
--- src/stores/useShufflerStore.js
+++ src/stores/useShufflerStore.js
@@ -1,55 +1,73 @@
 import create from 'zustand';
 
 const useShufflerStore = create((set) => ({
-  values: [],
-  resetValues: () => set(() => ({ values: [] })),
-  inputValue: '',
-  handleInputValue: (e) => set(() => {
-    return { inputValue: e.target.value }
-  }),
-  addChoice: () => set((state) => {
-    const newValues = [ ...state.values ];
-    newValues.push({
-      value: state.inputValue,
-      newValue: '',
-      isEditable: false,
-    });
-    return { inputValue: '', values: newValues };
-  }),
-  handleEditValue: (val, index) => set((state) => {
-    const arrCopied = [...state.values];
-    arrCopied[index].newValue = val;
-    return { values: arrCopied };
-  }),
-  editable: (index) => set((state) => {
-    const newArr = [...state.values];
-    newArr[index].isEditable = true;
-    return { values: newArr }
-  }),
-  acceptEdit: (index) => set((state) => {
-    const arrCopied = [...state.values];
-    arrCopied[index].value = arrCopied[index].newValue;
-    arrCopied[index].newValue = '';
-    arrCopied[index].isEditable = false;
-    return { values: arrCopied };
-  }),
-  declineEdit: (index) => set((state) => {
-    const newArr = [...state.values];
-    newArr[index].isEditable = false;
-    newArr[index].newValue = '';
-    return { values: newArr }
-  }),
-  deleteChoice: (index) => set((state) => {
-    const newValues = [ ...state.values ];
-    return { values: newValues.filter((choice) => choice.value !== state.values[index].value) };
-  }),
-  isShuffling: false,
-  handleShuffle: () => set((state) => ({ isShuffling: !state.isShuffling })),
-  isShuffleLoading: false,
-  loading: () => set((state) => ({ isShuffleLoading: true })),
-  notLoading: () => set((state) => ({ isShuffleLoading: false })),
-  result: '',
-  updateResult: (value) => set(() => ({ result: value })),
+    values: [],
+    resetValues: () => set(() => ({ values: [] })),
+    inputValue: '',
+    handleInputValue: (e) =>
+        set(() => {
+            return { inputValue: e.target.value };
+        }),
+    addChoice: () =>
+        set((state) => {
+            const newValues = [...state.values];
+            newValues.push({
+                value: state.inputValue,
+                newValue: '',
+                isEditable: false,
+            });
+            return { inputValue: '', values: newValues };
+        }),
+    handleEditValue: (val, index) =>
+        set((state) => {
+            const arrCopied = [...state.values];
+            arrCopied[index].newValue = val;
+            return { values: arrCopied };
+        }),
+    editable: (index) =>
+        set((state) => {
+            const newArr = [...state.values];
+            newArr[index].isEditable = true;
+            return { values: newArr };
+        }),
+    acceptEdit: (index) =>
+        set((state) => {
+            const arrCopied = [...state.values];
+
+            if (arrCopied[index].newValue.length !== 0) {
+                arrCopied[index].value = arrCopied[index].newValue;
+                arrCopied[index].newValue = '';
+                arrCopied[index].isEditable = false;
+                return { values: arrCopied };
+            }
+
+            arrCopied[index].newValue = '';
+            arrCopied[index].isEditable = false;
+            return { values: arrCopied };
+        }),
+    declineEdit: (index) =>
+        set((state) => {
+            const newArr = [...state.values];
+            newArr[index].isEditable = false;
+            newArr[index].newValue = '';
+            return { values: newArr };
+        }),
+    deleteChoice: (index) =>
+        set((state) => {
+            const newValues = [...state.values];
+            return {
+                values: newValues.filter(
+                    (choice) => choice.value !== state.values[index].value
+                ),
+            };
+        }),
+    isShuffling: false,
+    handleShuffle: () => set((state) => ({ isShuffling: !state.isShuffling })),
+    isShuffleLoading: false,
+    loading: () => set((state) => ({ isShuffleLoading: true })),
+    notLoading: () => set((state) => ({ isShuffleLoading: false })),
+    result: '',
+    updateResult: (value) => set(() => ({ result: value })),
 }));
 
 export default useShufflerStore;
